8ce8a54d3a10b49fee576250d21c0299066bb0cd
a2nfinance <levi@a2n.finance>
2023-07-23 23:40:59 +0700

    Add endpoints to get contract's files

--- webapp/src/pages/api/contract/getFiles.ts
+++ webapp/src/pages/api/contract/getFiles.ts
@@ -1,24 +1,18 @@
 import connect from 'src/database/connect';
 import { NextApiRequest, NextApiResponse } from 'next';
-import DAO from "src/database/models/DAO";
-
+import * as fs from "fs";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        const {
-            _id
-        } = req.body;
-        if (_id) {
-        
+            let {name} = req.body;
             try {
-                let dao = await DAO.findById(_id);
-                return res.status(200).send(dao);
+                let jsonFile = fs.readFileSync(`contract-files/${name}/${name}-contract-abi.json`);
+                let binFile = fs.readFileSync(`contract-files/${name}/${name}-contract.bin`);
+
+                return res.status(200).send({success: true, jsonFile: `${jsonFile}`, binFile: binFile});
             } catch (error) {
                 console.log(error)
                 return res.status(500).send(error.message);
             }
-        } else {
-            res.status(422).send('data_incomplete');
-        }
     } else {
         res.status(422).send('req_method_not_supported');
     }
